411fe7690a0126f911b57d2b11c5122721d234d4
Aryangarg014 <23bcs038@iiitdmj.ac.in>
2025-08-24 01:48:16 +0530

    Updated the goal Schema and goal controller.

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -22,6 +22,9 @@ async function createGoal(req, res){
         if(!description || description.trim() === ""){
             newGoal.description = "";
         }
+        else{
+            newGoal.description = description;
+        }
 
         if(groupId){
             const foundGroup = await Group.findById(groupId);
@@ -30,7 +33,7 @@ async function createGoal(req, res){
             }
 
             if(!foundGroup.members.includes(userId)){
-                return res.status(403).json( { message : "You are not a member of this group" } )
+                return res.status(403).json( { message : "You are not a member of this group" } );
             }
             newGoal.group = groupId;
         }
@@ -50,11 +53,66 @@ async function getUserGoals(req, res){
 }
 
 async function getGoalsByGroup(req, res){
+    const userId = req.user.id;
+    const groupId = req.params.id;
+    try{
+        const group = await Group.findById(groupId);
+        
+        if(!group){
+            return res.status(404).json( { message : "Group not found." } );
+        }
 
+        if(!group.members.includes(userId)){
+            return res.status(403).json( { message : "You are not a member of this group" } );
+        }
+
+        const goals = await Goal.find({group : groupId})
+                                .populate("createdBy", "name email")
+                                .populate("completedBy.user", "name email");
+
+        return res.status(200).json(goals);
+    }
+    catch(error){
+        console.error("Error fetching group goals: ", error.message);
+        res.status(500).send("Internal Server Error");
+    }
 }
 
 async function getGoalDetails(req, res){
+    const goalId = req.params.id;
+    const userId = req.user.id;
+    try{
+        // after populating createdBy and completedBy become objects.
+        const goal = await Goal.findById(goalId)
+                                .populate("createdBy", "name email")
+                                .populate("completedBy.user", "name email");
+        
+        if(!goal){
+            return res.status(404).json( { message : "Goal not found." } ); 
+        }
 
+        if(goal.group){     // Group Goal
+            const group = await Group.findById(goal.group);
+            if(!group){
+                return res.status(404).json( { message : "Group not found." } );
+            }
+
+            if(!group.members.includes(userId)){
+                return res.status(403).json( { message : "You are not a member of this group." } );
+            }
+        }
+        else{       // Personal Goal
+            if(goal.createdBy._id.toString() !== userId){
+                return res.status(403).json( { message : "This goal is not created by you." } );
+            }
+        }
+
+        return res.status(200).json(goal);
+    }
+    catch(error){
+        console.error("Error fetching goal details: ", error.message);
+        res.status(500).send("Internal Server Error");
+    }
 }
 
 async function markGoalComplete(req, res){
@@ -72,19 +130,26 @@ async function markGoalComplete(req, res){
                 return res.status(404).json( { message : "Group not found." } );
             }
             if(!group.members.includes(userId)){
-                return res.status(403).json( { message : "This goal is not assigned to you." } );
+                return res.status(403).json( { message : "You are not a member of this group." } );
             }
         }
         else if(goal.createdBy.toString() !== userId){      // Personal Goal
-            return res.status(403).json( { message : "This goal is not created by you." } )
+            return res.status(403).json( { message : "This goal is not created by you." } );
         }
 
-        if(goal.completedBy.includes(userId)){
+        let alreadyCompleted =  goal.completedBy.some((c) => c.user.toString() == userId );
+        if(alreadyCompleted){
             return res.status(400).json( { message : "You have already completed this goal." } );
         }
 
-        goal.completedBy.push(userId);
+        if(!goal.dueDate || (goal.dueDate && new Date() <= goal.dueDate)){
+            goal.completedBy.push({user : userId, status : "on-time"});
+        }
+        else{
+            goal.completedBy.push({user : userId, status : "late"});
+        }
         await goal.save();
+        
         return res.status(200).json( { message : "Goal marked completed successfully.", goal } );
     }
     catch(error){
@@ -126,8 +191,8 @@ async function deleteGoal(req, res){
             }
         }
 
-        await Goal.findByIdAndDelete(goalId);
-        return res.status(200).json( { message : "Goal deleted successfully." } );
+        const deletedGoal = await Goal.findByIdAndDelete(goalId);
+        return res.status(200).json({ message: "Goal deleted successfully.", goal: deletedGoal });
     }
     catch(error){
         console.error("Error deleting goal : ", error.message);
